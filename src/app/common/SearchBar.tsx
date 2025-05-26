381e87fbce1c59769857583857cd47a63d49dfb7
이한빈 <kx1302@naver.com>
2025-05-26 22:08:55 +0900

    [WEB-47] ✨ feat: mockData를를 활용한 검색 기능 구현

--- src/app/common/SearchBar.tsx
+++ src/app/common/SearchBar.tsx
@@ -1,20 +1,47 @@
 'use client';
 
+const mockData = [
+	{
+		id: 0,
+		title: '0506 TIL',
+		content: '0번 내용 입니다',
+	},
+	{
+		id: 1,
+		title: '0510 TIL',
+		content: '1번 내용 입니다',
+	},
+	{
+		id: 2,
+		title: '0516 TIL',
+		content: '2번 내용 입니다',
+	},
+];
+
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 
 export default function SearchBar() {
 	const [contents, setContents] = useState('');
 
-	const searchContents = (e: ChangeEvent<HTMLInputElement>) => {
-		console.log(e.target.value);
-		setContents(e.target.value);
+	const searchArticles = (event: ChangeEvent<HTMLInputElement>) => {
+		setContents(event.target.value);
+	};
+
+	const getFilteredArticles = () => {
+		if (contents === '') {
+			return mockData;
+		}
+		return mockData.filter(
+			(item) =>
+				item.content.includes(contents) || item.title.includes(contents),
+		);
 	};
 
 	return (
 		<>
-			<div className="w-[275] text-[18px] bg-[color:var(--color-ossca-mint-600)]/50 pretendard-500 rounded-[25px] pt-2 pb-2">
-				<div className="pl-4 pr-5 flex flex-row items-center justify-between">
+			<div className="w-[80%] text-[18px] bg-[color:var(--color-ossca-mint-600)]/50 pretendard-500 rounded-[25px] mt-2 pt-2 pb-2">
+				<div className="pl-[1rem] pr-[1.5rem] flex flex-row items-center justify-between">
 					<Image
 						src="/search-icon.png"
 						alt="검색 아이콘"
@@ -25,10 +52,15 @@ export default function SearchBar() {
 						type="text"
 						value={contents}
 						placeholder="Search"
-						onChange={searchContents}
-						className="w-[200] text-right focus:outline-none"
+						onChange={searchArticles}
+						className="w-[80%] text-right focus:outline-none"
 					/>
 				</div>
+			</div>
+			<div className="flex flex-col">
+				{getFilteredArticles().map((item) => (
+					<div key={item.id}>{item.title}</div>
+				))}
 			</div>
 		</>
 	);
