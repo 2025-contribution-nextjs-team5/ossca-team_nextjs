af13cf7167adc59273363059e22730b319d3c6db
이한빈 <kx1302@naver.com>
2025-06-01 15:56:01 +0900

    [WEB-47] ✨ feat: SearchBar, Search 페이지 분리

--- src/app/common/SearchBar.tsx
+++ src/app/common/SearchBar.tsx
@@ -49,7 +49,7 @@ export default function SearchBar() {
 	};
 
 	return (
-		<>
+		<div className="ml-[80%]">
 			<div className="w-[80%] text-[18px] bg-[color:var(--color-ossca-mint-600)]/50 pretendard-500 rounded-[25px] mt-2 pt-2 pb-2">
 				<div className="pl-[1rem] pr-[1.5rem] flex flex-row items-center justify-between">
 					<Image
@@ -68,13 +68,13 @@ export default function SearchBar() {
 					/>
 				</div>
 			</div>
-			<div className="flex flex-col mt-3">
+			{/* <div className="flex flex-col mt-3">
 				{getFilteredArticles().map((item) => (
 					<div key={item.id}>
 						{item.title} - {item.content}
 					</div>
 				))}
-			</div>
-		</>
+			</div> */}
+		</div>
 	);
 }
