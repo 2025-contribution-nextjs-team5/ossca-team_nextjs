846dda7746a871c4e1a2b59482175725a6e35848
이한빈 <kx1302@naver.com>
2025-06-02 21:55:38 +0900

    resolve: rebase 충돌 해결

---
+++ src/app/common/SortArticle.tsx
@@ -0,0 +1,11 @@
+export default function SortArticle() {
+	return (
+		<>
+			<div className="flex">
+				<div className="">정확도순</div>
+				<div className="ml-2 mr-2 text-gray-300">|</div>
+				<div className="text-gray-300">최신순</div>
+			</div>
+		</>
+	);
+}
