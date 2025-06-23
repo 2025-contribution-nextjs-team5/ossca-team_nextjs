5ef5a330894609cce8d1f0d944c0ad16e513c6d9
Clt689 <kx1302@naver.com>
2025-06-23 15:53:31 +0900

    fix: count prop 타입 추가

--- src/app/search/SearchResultCount.tsx
+++ src/app/search/SearchResultCount.tsx
@@ -1,4 +1,8 @@
-export default function SearchResultCount({ count }) {
+interface SearchResultCountProps {
+	count: number;
+}
+
+export default function SearchResultCount({ count }: SearchResultCountProps) {
 	return (
 		<div className="w-[120px] ml-[5%] text-center mb-4 mt-3 pt-2 pb-2 rounded-[0.3vw] font-bold text-xl text-white bg-ossca-mint-300/60">
 			{count} Results
